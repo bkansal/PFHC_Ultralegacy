c478b4a57eb6a7509ace5d7a14ea109adb892011
bkansal <bhumika.kansal@cern.ch>
2022-09-20 12:09:20 +0530

    ovarlay scripts v2

--- final_codes/genrateplot_resovsE_UL.C
+++ final_codes/genrateplot_resovsE_UL.C
@@ -1,7 +1,7 @@
 #include <stdio.h>
 // #include<conio.h>
 
-void genrateplot_resovsE_UL(string legendname,string plot_name, bool xaxis_pt, bool raw)
+void genrateplot_resovsE_UL(string legendname,string plot_name, bool xaxis_pt, bool corrE=false,   bool raw=true)
 {
 //=========Macro generated from canvas: Canvas_1_n2/Canvas_1_n2                                                                                                                   
 //=========  (Mon Apr  1 19:35:54 2019) by ROOT version 6.14/06
@@ -30,42 +30,91 @@ void genrateplot_resovsE_UL(string legendname,string plot_name, bool xaxis_pt, b
   char* rootfile3 = new char[2000];
 
 
-  sprintf(path_root1,"/Users/bkansal/work/Ultra_legacy/final/for_UL2016/trial1/response_plots_root");
-  sprintf(path_root2,"/Users/bkansal/work/Ultra_legacy/final/for_UL2017/trial1/response_plots_root/before15may");
-  sprintf(path_root3,"/Users/bkansal/work/Ultra_legacy/final/for_UL2018/trial1/response_plots_root");
+  sprintf(path_root1,"UL2016/response");
+  sprintf(path_root2,"UL2017/response");
+  sprintf(path_root3,"UL2018/response");
+
   sprintf(rootfile1,"correta");
   sprintf(rootfile2,"correta");
   sprintf(rootfile3,"correta");
   sprintf(hname,"%s/%s_%s_10_6_0.root",path_root1,rootfile1,plot_name.c_str());
   sprintf(hname1,"%s/%s_%s_10_6_0_pre2.root",path_root2,rootfile2,plot_name.c_str());
   sprintf(hname2,"%s/%s_%s_10_6_0.root",path_root3,rootfile3,plot_name.c_str());
-  sprintf(path,"/Users/bkansal/work/Ultra_legacy/final/plots/corrected/overlay_rawwrtUL_calib/responseplots/%s_reso.gif",plot_name.c_str());
-  sprintf(path1,"/Users/bkansal/work/Ultra_legacy/final/plots/corrected/overlay_rawwrtUL_calib/responseplots/%s_reso.pdf",plot_name.c_str());
-  sprintf(path2,"/Users/bkansal/work/Ultra_legacy/final/plots/corrected/overlay_rawwrtUL_calib/responseplots/%s_reso.png",plot_name.c_str());
 
+  if(corrE){
+    sprintf(rootfile1,"corrE");
+    sprintf(rootfile2,"corrE");
+    sprintf(rootfile3,"corrE");
+    sprintf(hname,"%s/%s_%s_10_6_0.root",path_root1,rootfile1,plot_name.c_str());
+    sprintf(hname1,"%s/%s_%s_10_6_0.root",path_root2,rootfile2,plot_name.c_str());
+    sprintf(hname2,"%s/%s_%s_10_6_0.root",path_root3,rootfile3,plot_name.c_str());
+  }
+  
   TFile * inputfile1 = new TFile(hname,"READ");
   TFile * inputfile2 = new TFile(hname1,"READ");
   TFile * inputfile3 = new TFile(hname2,"READ");
   TGraph* graph1 = (TGraph*) inputfile1 -> Get("Graph;2");
-  TGraph* graph2 = (TGraph*) inputfile2 -> Get("Graph;2");
+  TGraph* graph2;
+  if(corrE)graph2 = (TGraph*) inputfile2 -> Get("Graph0;1");
+  else  graph2 = (TGraph*) inputfile2 -> Get("Graph;2");
   TGraph* graph3 = (TGraph*) inputfile3 -> Get("Graph;2");
   TH2F *respHisto__1 = (TH2F*) inputfile1 -> Get("resoHisto");
-
-
-  sprintf(path_root1,"/Users/bkansal/work/Ultra_legacy/final/for_UL2016/raw/response_plots_root");
-  sprintf(path_root2,"/Users/bkansal/work/Ultra_legacy/final/for_UL2017/raw/response_plots_root");
-  sprintf(path_root3,"/Users/bkansal/work/Ultra_legacy/final/for_UL2018/trial1/response_plots_root");
   sprintf(rootfile1,"Raw");
   sprintf(rootfile2,"Raw");
   sprintf(rootfile3,"Raw");
   sprintf(hname,"%s/%s_%s_10_6_0_pre2_response.root",path_root1,rootfile1,plot_name.c_str());
   sprintf(hname1,"%s/%s_%s_10_6_0_pre2.root",path_root2,rootfile2,plot_name.c_str());
   sprintf(hname2,"%s/%s_%s_10_6_0_pre2_response.root",path_root3,rootfile3,plot_name.c_str());
-  sprintf(path,"/Users/bkansal/work/Ultra_legacy/final/plots/corrected/overlay_rawwrtUL_calib/responseplots/%s_reso.gif",plot_name.c_str());
-  sprintf(path1,"/Users/bkansal/work/Ultra_legacy/final/plots/corrected/overlay_rawwrtUL_calib/responseplots/%s_reso.pdf",plot_name.c_str());
-  sprintf(path2,"/Users/bkansal/work/Ultra_legacy/final/plots/corrected/overlay_rawwrtUL_calib/responseplots/%s_reso.png",plot_name.c_str());
+  
+  if(corrE && !raw){
+    sprintf(path,"plots/corrE_%s_reso.gif",plot_name.c_str());
+    sprintf(path1,"plots/corrE_%s_reso.pdf",plot_name.c_str());
+    sprintf(path2,"plots/corrE_%s_reso.png",plot_name.c_str());
+  }
+  else if(!corrE && raw){
+    sprintf(path,"plots/raw_%s_reso.gif",plot_name.c_str());
+    sprintf(path1,"plots/raw_%s_reso.pdf",plot_name.c_str());
+    sprintf(path2,"plots/raw_%s_reso.png",plot_name.c_str());
+  }  
+  else if(!raw && !corrE){
+    sprintf(path,"plots/corrEtawrtraw_%s_reso.gif",plot_name.c_str());
+    sprintf(path1,"plots/corrEtawrtraw_%s_reso.pdf",plot_name.c_str());
+    sprintf(path2,"plots/corrEtawrtraw_%s_reso.png",plot_name.c_str());
+  }
+  else if(raw && corrE){
+    sprintf(path,"plots/corrEwrtraw_%s_reso.gif",plot_name.c_str());
+    sprintf(path1,"plots/corrEwrtraw_%s_reso.pdf",plot_name.c_str());
+    sprintf(path2,"plots/corrEwrtraw_%s_reso.png",plot_name.c_str());
+  }
 
-
+  if(xaxis_pt && !raw && !corrE){ 
+    sprintf(path_root1,"UL2016/response");
+    sprintf(path_root2,"UL2017/response");
+    sprintf(path_root3,"UL2018/response");
+    sprintf(rootfile1,"correta");
+    sprintf(rootfile2,"correta");
+    sprintf(rootfile3,"correta");
+    sprintf(hname,"%s/%s_%s_10_6_0_pt.root",path_root1,rootfile1,plot_name.c_str()); 
+    sprintf(hname1,"%s/%s_%s_10_6_0_pre2_pt.root",path_root2,rootfile2,plot_name.c_str());
+    sprintf(hname2,"%s/%s_%s_10_6_0_pt.root",path_root3,rootfile3,plot_name.c_str());
+    inputfile1 = new TFile(hname,"READ");
+    inputfile2 = new TFile(hname1,"READ");
+    inputfile3 = new TFile(hname2,"READ");
+    graph1 = (TGraph*) inputfile1 -> Get("Graph;2");
+    graph2 = (TGraph*) inputfile2 -> Get("Graph;2");
+    graph3 = (TGraph*) inputfile3 -> Get("Graph;2");
+    respHisto__1 = (TH2F*) inputfile1 -> Get("resoHisto");
+    sprintf(rootfile1,"Raw");
+    sprintf(rootfile2,"Raw");
+    sprintf(rootfile3,"Raw");
+    sprintf(hname,"%s/%s_%s_10_6_0_pre2_response_pt.root",path_root1,rootfile1,plot_name.c_str());
+    sprintf(hname1,"%s/%s_%s_10_6_0_pre2_pt.root",path_root2,rootfile2,plot_name.c_str());
+    sprintf(hname2,"%s/%s_%s_10_6_0_pre2_response_pt.root",path_root3,rootfile3,plot_name.c_str());    
+    sprintf(path,"plots/corrEtawrtraw_%s_reso_pt.gif",plot_name.c_str());
+    sprintf(path1,"plots/corrEtawrtraw_%s_reso_pt.pdf",plot_name.c_str());
+    sprintf(path2,"plots/corrEtawrtraw_%s_reso_pt.png",plot_name.c_str());
+  } 
+  
   cout<<"file name --> "<<plot_name<<endl;  
    TCanvas *Canvas_1_n2 = new TCanvas("Canvas_1_n2", "Canvas_1_n2",65,50,650,500);
    //   Canvas_1_n2->Range(-60.25,-0.625,562.25,0.625);
@@ -79,8 +128,15 @@ void genrateplot_resovsE_UL(string legendname,string plot_name, bool xaxis_pt, b
    TFile * inputfile1_ = new TFile(hname,"READ");
    TFile * inputfile2_ = new TFile(hname1,"READ");
    TFile * inputfile3_ = new TFile(hname2,"READ");
-   TGraph* graph1_ = (TGraph*) inputfile1_ -> Get("Graph;2");
-   TGraph* graph2_ = (TGraph*) inputfile2_ -> Get("Graph;2");
+   TGraph *graph1_,*graph2_;
+   if(xaxis_pt) {
+     graph1_ = (TGraph*) inputfile1_ -> Get("resolution;1");
+     graph2_ = (TGraph*) inputfile2_ -> Get("resolution;1");
+   }
+   else {
+     graph1_ = (TGraph*) inputfile1_ -> Get("Graph;2");
+     graph2_ = (TGraph*) inputfile2_ -> Get("Graph;2");
+   }
    TGraph* graph3_ = (TGraph*) inputfile3_ -> Get("Graph;2");
 
    // respHisto__1->SetStats(0);
@@ -95,21 +151,21 @@ void genrateplot_resovsE_UL(string legendname,string plot_name, bool xaxis_pt, b
    graph1->SetMarkerColor(kRed);
    graph2->SetMarkerColor(kBlue);
    graph3->SetMarkerColor(kGreen+3);
-   graph1_->SetMarkerColor(kRed);
-   graph2_->SetMarkerColor(kBlue);
-   graph3_->SetMarkerColor(kGreen+3);
-   /*
-   graph1_->SetMarkerStyle(26);
-   graph2_->SetMarkerStyle(26);
-   graph3_->SetMarkerStyle(26);
-
    graph3->Draw("p");
    graph2->Draw("p");
    graph1->Draw("p");
-   */
-   graph3_->Draw("p");
-   graph2_->Draw("p");
-   graph1_->Draw("p");
+
+   if((!raw && !corrE) || (raw && corrE)){     
+     graph1_->SetMarkerColor(kRed);
+     graph2_->SetMarkerColor(kBlue);
+     graph3_->SetMarkerColor(kGreen+3);
+     graph1_->SetMarkerStyle(26);
+     graph2_->SetMarkerStyle(26);
+     graph3_->SetMarkerStyle(26);
+     graph3_->Draw("p");
+     graph2_->Draw("p");
+     graph1_->Draw("p");
+   }
 
 
    // TLine *line = new TLine(1.2273,0,498.0851,0);
@@ -126,17 +182,39 @@ void genrateplot_resovsE_UL(string legendname,string plot_name, bool xaxis_pt, b
   //  if(isPaper) textOnTop.DrawLatexNDC(0.12,0.91,"CMS #it{#bf{Simulation Supplementary}}")
    textOnTop.DrawLatexNDC(0.1,0.91,"CMS #it{#bf{Simulation Preliminary}}");
 
-   TLegend* legends = new TLegend(0.18, 0.7, 0.9, 0.9,"","brNDC"); // the numbers determine the position of the box                                                                          
+   TLegend* legends = new TLegend(0.12, 0.7, 0.9, 0.9,"","brNDC"); // the numbers determine the position of the box                                                                          
    legends->SetFillColor(0);
    legends->SetHeader(legendname.c_str(),"C");
-   //legends->SetNColumns(2);
-   //   legends->AddEntry(graph1,"2016 (After calibration)","P");//(name of hist,what you want it called in legend, l=line, p=polymarker, f=boxy thing )
-   legends->AddEntry(graph1_,"2016 (Before calibration)","P");//(name of hist,what you want it called in legend, l=line, p=polymarker, f=boxy thing )
-   //legends->AddEntry(graph2,"2017 (After calibration)","P");//(name of hist,what you want it called in legend, l=line, p=polymarker, f=boxy thing )                                        
-   legends->AddEntry(graph2_,"2017 (Before calibration)","P");//(name of hist,what you want it called in legend, l=line, p=polymarker, f=boxy thing )                                       
-   //legends->AddEntry(graph3,"2018 (After calibration)","P");
-   legends->AddEntry(graph3_,"2018 (Before calibration)","P");
+   if((!raw && !corrE) || (xaxis_pt && !raw && !corrE)){
+     legends->SetNColumns(2);
+     legends->AddEntry(graph1,"2016 (After calibration)","P");//(name of hist,what you want it called in legend, l=line, p=polymarker, f=boxy thing )
+     legends->AddEntry(graph1_,"2016 (Before calibration)","P");//(name of hist,what you want it called in legend, l=line, p=polymarker, f=boxy thing )
+     legends->AddEntry(graph2,"2017 (After calibration)","P");//(name of hist,what you want it called in legend, l=line, p=polymarker, f=boxy thing )
+     legends->AddEntry(graph2_,"2017 (Before calibration)","P");//(name of hist,what you want it called in legend, l=line, p=polymarker, f=boxy thing )
+     legends->AddEntry(graph3,"2018 (After calibration)","P");
+     legends->AddEntry(graph3_,"2018 (Before calibration)","P");
+   }
+   else if(raw && corrE){
+     legends->SetNColumns(2);
+     legends->AddEntry(graph1,"2016 (After E calibration)","P");//(name of hist,what you want it called in legend, l=line, p=polymarker, f=boxy thing )
+     legends->AddEntry(graph1_,"2016 (Before calibration)","P");//(name of hist,what you want it called in legend, l=line, p=polymarker, f=boxy thing )
+     legends->AddEntry(graph2,"2017 (After E calibration)","P");//(name of hist,what you want it called in legend, l=line, p=polymarker, f=boxy thing )
+     legends->AddEntry(graph2_,"2017 (Before calibration)","P");//(name of hist,what you want it called in legend, l=line, p=polymarker, f=boxy thing )
+     legends->AddEntry(graph3,"2018 (After E calibration)","P");
+     legends->AddEntry(graph3_,"2018 (Before calibration)","P");
+   }
 
+   else if(raw && !corrE){
+     legends->AddEntry(graph1,"2016 (Before calibration)","P");
+     legends->AddEntry(graph2,"2017 (Before calibration)","P");
+     legends->AddEntry(graph3,"2018 (Before calibration)","P");
+   }
+   else{
+     legends->AddEntry(graph1,"2016 (After E calibration)","P");
+     legends->AddEntry(graph2,"2017 (After E calibration)","P");
+     legends->AddEntry(graph3,"2018 (After E calibration)","P");
+   }
+   
    legends->SetTextSize(0.04);
    //   legends->SetMarkerStyle(1);
    legends->Draw();
